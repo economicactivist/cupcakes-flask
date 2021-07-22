8058dfc08b547f6354ac8ce46710a9bef0d48e0a
Mountain Scott <mountainscott@yahoo.com>
2021-07-21 19:47:11 -0700

    "getting an error for the "ul" element

--- static/index.js
+++ static/index.js
@@ -1,8 +1,42 @@
-// $(document).ready()
+
+
+    // let list = document.querySelector('#cupcakes');
+    // let cupcakeList = list.querySelectorAll('li');
+    // let cupcakeCount = cupcakeList.length;
+    // let cupcakes = [];
+    // for (let i = 0; i < cupcakeCount; i++) {
+    //     cupcakes.push(cupcakeList[i].innerHTML);
+    // }
+    // return cupcakes;
+
+
+
+async function getCupcakes() {
+    let cupcakeList = await fetch('api/cupcakes').then(res => res.json());
+    return cupcakeList;
+}
+
+$(document).ready(() => {
+    let cupcakes = getCupcakes();
+    let list = document.querySelector('#cupcakes');
+    let ul = list.querySelector('ul');
+    ul.innerHTML = '';
+    cupcakes.forEach(cupcake => {
+        let li = document.createElement('li');
+        li.innerHTML = cupcake;
+        ul.appendChild(li);
+    });
+    alert('cupcake list loaded')
+});
+
+    
+
+
+
 $( "#form" ).submit(function( event, addCupcake) {
     alert( "Handler for .submit() called." )
     event.preventDefault()
-    let $form = $( this )
+    let $form = $( this ), 
         $select = $(".rating")
 
     async function addCupcake() {
